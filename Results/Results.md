f77e4c6e266046e8a30fefc67bdcd1e24f75f84b
Gustav Radbrandt <gustav.radbrandt@gmail.com>
2022-05-29 16:30:18 +0200

    analyze.py run on POP_OS, minor changes adn result

---
+++ Results/Results.md
@@ -0,0 +1,4 @@
+# Results from before and after transfer learning
+## Original dataset and training
+### Accurac for validation data:
+97.299%
--- analyze.py
+++ analyze.py
@@ -40,7 +40,7 @@ def predict(img_dir, model):
         pred_label = decoded[0][0][0]
         # print(pred_label)
         y_pred.append(pred_label)
-        tokens = img_path.split(os.pathsep)
+        tokens = img_path.split('/')
         class_id = int(tokens[-2])
         # print(str(class_id))
         y_test.append(class_id)
@@ -125,3 +125,4 @@ if __name__ == '__main__':
                           title='Normalized confusion matrix')
 
     plt.show()
+    pass
